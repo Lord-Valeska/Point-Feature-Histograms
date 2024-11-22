a3d239e776776d34c00511d009d2e8182b385dc9
Lord-Valeska <frederick21jiang@gmail.com>
2024-11-22 00:17:12 -0500

    PFH completed

--- pfh.py
+++ pfh.py
@@ -4,13 +4,23 @@ import numpy
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.spatial import KDTree
+from scipy.special import comb
 
 class PFH(object):
-    def __init__(self, pointcloud, radius, num_neighbors):
-        self.pc = np.asarray(pointcloud).T # (N, 3)
+    def __init__(self, pointcloud, radius, num_neighbors=8, div=2, num_features=4):
+        self.pc = pointcloud # (N, 3)
         self.r = radius
         self.nneighbors = num_neighbors
         self.tree = None
+        self.normals = self.get_normals()
+        self.div = div
+        self.nfeatures = num_features
+    
+    def get_point(self, idx):
+        return self.pc[idx]
+    
+    def get_size(self):
+        return self.pc.shape[0]
 
     def pca_normal(self, X):
         """
@@ -24,27 +34,34 @@ class PFH(object):
         normal = V_T.T[:, -1]
         return normal
 
-    def get_kNNs(self, point):
+    def get_kNNs(self, idx):
         """
         Get k-neighborhood defined by a sphere centered at point with self.radius in self.pc
 
         PARAMS:
-        point - the point of interest
+        idx - point index in self.pc
 
         RETURN:
         ndarray of k points
         """
         if self.tree is None:
             self.tree = KDTree(self.pc)
-        
+
+        point = self.pc[idx]
         idx_within_radius = self.tree.query_ball_point(point, r=self.r)
-        if idx_within_radius:
-            points_within_radius = self.pc[idx_within_radius[0]]
-            tree_within_radius = KDTree(points_within_radius)
-            distances, k_indices = tree_within_radius.query(point, k=min(self.nneighbors, len(points_within_radius)))
-            neighborhood = np.asarray(points_within_radius)[k_indices]
-            # print(f"{self.nneighbors}-neighborhood within radius {self.r}: {neighborhood}")
-            return neighborhood.squeeze()
+        # Exclude the point itself
+        idx_within_radius = [i for i in idx_within_radius if i != idx]
+    
+        if idx_within_radius is not None:
+            if len(idx_within_radius) > self.nneighbors:
+                points_within_radius = self.pc[idx_within_radius]
+                tree_within_radius = KDTree(points_within_radius)
+                distances, k_indices = tree_within_radius.query(point, k=self.nneighbors)
+                neighbor_indices = np.asarray(idx_within_radius)[k_indices]
+            else:
+                neighbor_indices = np.asarray(idx_within_radius)
+            # print(f"{idx}: {self.nneighbors}-neighborhood within radius {self.r}: {neighbor_indices}")
+            return np.asarray(neighbor_indices)
         else:
             print("No points within radius")
             return None
@@ -60,7 +77,8 @@ class PFH(object):
         N = self.pc.shape[0]
         mean = np.mean(self.pc, axis=0)
         for i in range(N):
-            neighbors = self.get_kNNs(self.pc[i].reshape(1,3))
+            neighbor_indices = self.get_kNNs(i)
+            neighbors = self.pc[neighbor_indices]
             normal = self.pca_normal(neighbors)
             v = mean - self.pc[i]
             v /= np.linalg.norm(v)
@@ -68,3 +86,152 @@ class PFH(object):
                 normal *= -1
             normals.append(normal)
         return np.asarray(normals)
+    
+    def get_features(self, idx):
+        """
+        idx - point index in self.pc
+
+        RETURN:
+        ndarray of size (self.nneighbors, self.nfeatures).
+        """
+        neighbor_indices = self.get_kNNs(idx)
+        n_combinations = comb((len(neighbor_indices) + 1), 2)
+        combined_list = [idx] + list(neighbor_indices)
+        points_idx = np.array(combined_list, dtype=int)
+        points_idx_copy = points_idx.copy()
+        features = []
+        for i_idx in points_idx:
+            p_i = self.pc[i_idx]
+            n_i = self.normals[i_idx]
+            points_idx_copy = points_idx_copy[1:]
+            for j_idx in points_idx_copy:
+                p_j = self.pc[j_idx]
+                n_j = self.normals[j_idx]
+                if np.arccos(np.dot(n_i, p_j - p_i)) <= np.arccos(np.dot(n_j, p_i - p_j)):
+                    source_idx = i_idx
+                    source_point = p_i
+                    target_idx = j_idx
+                    target_point = p_j
+                else:
+                    source_idx = j_idx
+                    source_point = p_j
+                    target_idx = i_idx
+                    target_point = p_i
+                d = np.linalg.norm(target_point - source_point)
+                # Construct the Darboux frame
+                u = self.normals[source_idx]
+                v = np.cross((target_point - source_point), u)
+                w = np.cross(u, v)
+                # Construct the four features (including the distance)
+                nt = self.normals[target_idx]
+                f1 = np.dot(v, nt)
+                f2 = d
+                f3 = np.dot(u, (target_point - source_point) / d)
+                f4 = np.arctan(np.dot(w, nt) / np.dot(u, nt))
+                if self.nfeatures == 4:
+                    features.append(np.array([f1, f2, f3, f4]))
+                elif self.nfeatures == 3:
+                    features.append(np.array([f1, f3, f4]))
+        features = np.asarray(features)
+        return features
+
+    def get_feature_histogram(self, idx):
+        """
+        Implemented 2 div, inclduing d in the feature.
+        Try 3 div without distance.
+        """
+        features = self.get_features(idx)
+        r = 0
+        if self.nfeatures == 4:
+            r = np.median(features[:, 1])
+        histogram = np.zeros(self.div ** self.nfeatures)
+        s = self.get_threshold(r)
+        for j in range(0, features.shape[0]):
+            index = 0
+            for i in range(0, self.nfeatures):
+                index += self.step(s[i], features[j, i]) * (self.div ** (i))
+            histogram[index] += 1
+        histogram /= features.shape[0]
+        return histogram
+
+    def get_threshold(self, r=0):
+        delta = 2. / self.div
+        s1 = np.array([-1 + i * delta for i in range(1, self.div)])
+        s3 = np.array([-1 + i * delta for i in range(1, self.div)])
+
+        delta = np.pi / self.div
+        s4 = np.array([-np.pi / 2 + i * delta for i in range(1, self.div)])
+
+        if self.nfeatures == 3:
+            s = np.array([s1, s3, s4])
+        elif self.nfeatures == 4:
+            s2 = np.array([r])
+            s = np.array([s1, s2, s3, s4])
+        return s
+    
+    def step(self, si, fi):
+        if self.div == 2:
+            result = 0 if fi < si[0] else 1
+        elif self.div == 3:
+            if fi < si[0]:
+                result = 0
+            elif fi >= si[0] and fi < si[1]:
+                result = 1
+            else:
+                result = 2
+        return result
+    
+    def get_all_histograms(self):
+        N = self.pc.shape[0]
+        histograms = np.zeros((N, self.div ** self.nfeatures))
+        for i in range(N):
+            histograms[i] = self.get_feature_histogram(i)
+        return histograms
+    
+    def transform(self, R, t):
+        self.pc = (R @ self.pc.T + t).T
+        self.tree = None
+        self.normals = self.get_normals()
+        return self.pc
+
+def get_correspondence(pfh_source, pfh_target):
+    C = []
+    histogram_source = pfh_source.get_all_histograms()
+    histogram_target = pfh_target.get_all_histograms()
+    for i in range(pfh_source.get_size()):
+        p = pfh_source.get_point(i)
+        histogram_p= histogram_source[i]
+        epsilon = 1e-10
+        numerator = (histogram_target - histogram_p) ** 2
+        denominator = histogram_target + histogram_p + epsilon
+        chi_squared_distances = np.sum(numerator / denominator, axis=1)
+        min_index = np.argmin(chi_squared_distances)
+        q = pfh_target.get_point(min_index)
+        C.append([p, q])
+    C = np.asarray(C) # (N, 2, 3)
+    return C
+
+def get_transform(C):
+    # Step 0
+    Cp = C[:, 0, :] # (N, 3)
+    Cq = C[:, 1, :] # (N, 3)
+    # Step 1
+    p_bar = np.mean(Cp, axis=0)
+    q_bar = np.mean(Cq, axis=0)
+    X = Cp - p_bar
+    Y = Cq - q_bar
+    # Step 2
+    S = X.T @ Y
+    U, sigma, V_T = np.linalg.svd(S)
+    # Step 3
+    M = np.eye(3)
+    M[2, 2] = np.linalg.det(V_T.T @ U.T)
+    R = V_T.T @ M @ U.T
+    t = q_bar.reshape(3, 1) - R @ p_bar.reshape(3, 1)
+    return R, t
+
+def get_error(C, R, t):
+    Cp = C[:, 0, :] # (N, 3)
+    Cq = C[:, 1, :] # (N, 3)
+    errors = (((R @ Cp.T).T + t.reshape(1, 3)) - Cq) ** 2
+    return np.sum(errors)
--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import numpy
 import matplotlib.pyplot as plt
 import numpy as np
 
-from pfh import PFH
+from pfh import PFH, get_correspondence, get_transform, get_error
 
 if __name__ == '__main__':
     # Load point clouds
@@ -17,20 +17,68 @@ if __name__ == '__main__':
     P = utils.convert_pc_to_matrix(pc_source)
     Q = utils.convert_pc_to_matrix(pc_target)
 
+    # Make a ndarray version of the point clouds
+    source = np.asarray(P).T
+    target = np.asarray(Q).T
+
     # Test for kNNs
-    k = 16
+    k = 8
     r = 0.03
-    pfh_source = PFH(P, r, k)
-    neighbors = np.matrix(pfh_source.get_kNNs(P[:, 0].squeeze()).T)
-    pc_neighbors = utils.convert_matrix_to_pc(neighbors)
-    
-    fig = utils.view_pc([pc_source, pc_neighbors], None, ['b', 'r'], ['^', 'o'])
+    pfh_source = PFH(source, r, k, 2, 4)
+    neighbor_indices = pfh_source.get_kNNs(216)
+    neighbors = source[neighbor_indices]
+    pc_neighbors = utils.convert_matrix_to_pc(np.matrix(neighbors.T))
+
+    fig = utils.view_pc([pc_source, pc_target], None, ['b', 'r'], ['^', 'o'])
 
     # Test for getting normals
-    normals = pfh_source.get_normals()
-    for i in range(len(normals)):
-        utils.draw_vector(fig, normals[i].squeeze(), P[:, i], color='y')
+    # normals = pfh_source.get_normals()
+    # for i in range(len(normals)):
+    #     utils.draw_vector(fig, normals[i].squeeze(), P[:, i], color='y')
 
     plt.axis([-0.15, 0.15, -0.15, 0.15, -0.1, 0.1])
     plt.show()
+
+    # Test for calculating features for a point
+    # features = pfh_source.get_features(216)
+    # print(features)
+
+    # Test for getting a histogram for a point
+    # histogram = pfh_source.get_feature_histogram(216)
+    # bins = np.arange(1, len(histogram) + 1)
+    # bar_width = 0.5
+    # plt.figure()
+    # plt.bar(bins, histogram, width=bar_width, color='red', label='$P_1$', edgecolor='black')
+    # plt.xlabel('Bins')
+    # plt.ylabel('Ratio of points in one bin (%)')
+    # plt.title('Persistent Feature Points Histograms for 3D Geometric Primitives')
+    # plt.grid(axis='y', linestyle='--', alpha=0.7)
+    # plt.xticks(bins)
+    # plt.show()
+
+    # Test for getting correspondence
+    threshold=0.0004683
+    k = 8
+    r = 0.03
+    pfh_source = PFH(source, r, k, 2, 4)
+    pfh_target = PFH(target, r, k, 2, 4)
+
+    while True:
+        C = get_correspondence(pfh_source, pfh_target)
+        R, t = get_transform(C)
+        aligned = pfh_source.transform(R, t)
+        error = get_error(C, R, t)
+        print(error)
+        if error < threshold:
+            break
+    
+    
+    pc_aligned = utils.convert_matrix_to_pc(np.matrix(aligned.T))
+    utils.view_pc([pc_aligned, pc_target], None, ['b', 'r'], ['o', '^'])
+    plt.axis([-0.15, 0.15, -0.15, 0.15, -0.1, 0.1])
+    plt.show()
+
+    
+
+    
     
